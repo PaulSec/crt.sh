144182e5548bf31f12c9154bf714bd68a32a14e3
Paul A <aos.paul@gmail.com>
2017-11-02 19:03:19 +0100

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-
 setup(
     name='crtsh',
+    packages = ['crtsh'],
     author='PaulSec',
     version='0.1.0',
     url='https://github.com/PaulSec/crt.sh',
     packages='.',
     description='(Unofficial) Python API for https://crt.sh',
+    install_requires=["bs4", "requests"],
+    url = 'https://github.com/PaulSec/crt.sh',
+    keywords = ['crt.sh', 'ssl', 'certificates', 'osint'],
 )
