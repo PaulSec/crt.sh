4231675e1ea149b33df7bcabf73d2fcd5af23892
Paul A <aos.paul@gmail.com>
2017-11-02 19:04:51 +0100

    modified setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(
     description='(Unofficial) Python API for https://crt.sh',
     install_requires=["bs4", "requests"],
     url = 'https://github.com/PaulSec/crt.sh',
+    download_url = 'https://github.com/PaulSec/crt.sh/archive/0.1.0.tar.gz',
     keywords = ['crt.sh', 'ssl', 'certificates', 'osint'],
 )
