09cba8ab634302b05e29066b5191453c970ec78f
Paul A <aos.paul@gmail.com>
2021-02-25 22:03:25 +0100

    Ready for 0.3.1 version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='crtsh',
     author='PaulSec',
-    version='0.3.0',
+    version='0.3.1',
     packages='.',
     description='(Unofficial) Python API for https://crt.sh',
     install_requires=["requests"],
     url = 'https://github.com/PaulSec/crt.sh',
-    download_url = 'https://github.com/PaulSec/crt.sh/archive/0.3.0.tar.gz',
+    download_url = 'https://github.com/PaulSec/crt.sh/archive/0.3.1.tar.gz',
     keywords = ['crt.sh', 'ssl', 'certificates', 'osint'],
 )
